0327bd7df0d89533a08d1dfbb7a1928e091ac6ef
musmany <mu052609@gmail.com>
2025-01-16 04:44:15 +0500

    2nd update
    Smit hackathon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './index.html', // Specify the path to your HTML file
+    './style.css',  // If you have CSS files that use Tailwind
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customColor: '#000000', // Add your custom color here
+      },
+      boxShadow: {
+        'bottom-only': '0 2px 4px rgba(0, 0, 0, 0.1)', // Adjust values as needed (no semicolon)
+      },
+    },
   },
   plugins: [],
 }
-
